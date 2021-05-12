9f899237e8e2f9b2f14aa9a4dc5d581ff4e8602a
Chih Sean Hsu <s8901489@gmail.com>
2021-05-12 15:48:24 +0800

    Update install_docker_compose.sh

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-set -e
 sh_c='sudo -E sh -c'
-
 $sh_c "curl -L 'https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose"
 $sh_c "chmod +x /usr/local/bin/docker-compose"
