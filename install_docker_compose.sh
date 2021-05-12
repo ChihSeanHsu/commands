11f7fb20e55684eb71979ac71db87a0328a6f3fd
Chih Sean Hsu <s8901489@gmail.com>
2021-05-12 15:12:43 +0800

    Update install_docker_compose.sh

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 set -e
-user="$(id -un 2>/dev/null || true)"
-sh_c='sh -c'
-if [ "$user" != 'root' ]; then
-  if command_exists sudo; then
-    sh_c='sudo -E sh -c'
-  elif command_exists su; then
-    sh_c='su -c'
-  else
-    cat "cannot run su can sudo"
-    exit 1
-  fi
-fi
+sh_c='sudo -E sh -c'
 
 $sh_c "curl -L 'https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose"
 $sh_c "chmod +x /usr/local/bin/docker-compose"
